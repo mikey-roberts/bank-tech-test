e319896a14a8ec1432b3a4279abde33fb28e468f
mikey-roberts <mikey_roberts@outlook.com>
2021-08-02 16:27:11 +0100

    ran rubocop to clean-up

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,54 +1,83 @@
+# frozen_string_literal: true
+
 class Bank
   attr_reader :account_balance, :transaction_history
+
   # Initializes with account balance of 0
   def initialize
     @account_balance = 0
     @transaction_history = []
   end
+
   # Actions a bank depoit
   def deposit_transaction(transaction_amount)
-    credit(transaction_amount)
-    save_transaction("#{transaction_date} || #{add_two_decimals(transaction_amount)} || || #{add_two_decimals(@account_balance)}")
+    validate_amount(transaction_amount)
+    credit_account(transaction_amount)
+    save_transaction(deposit_format(transaction_amount))
   end
+
   # Actions a bank withdraw
   def withdrawal_transaction(transaction_amount)
-    debit(transaction_amount)
-    save_transaction("#{transaction_date} || || #{add_two_decimals(transaction_amount)} || #{add_two_decimals(@account_balance)}")
+    validate_amount(transaction_amount)
+    debit_account(transaction_amount)
+    save_transaction(withdrawal_format(transaction_amount))
   end
+
   # Provides a statement with a summary of all transactions
   def statement_summary
     statement_header
     statement_body
   end
-  
+
   private
+
   # Converts values to display two decimal places
   def add_two_decimals(value)
-    '%.2f' % value
+    ('%.2f' % value)
   end
+
   # Heading text for summary STDout
   def statement_header
-    puts "date || credit || debit || balance"
+    puts 'date || credit || debit || balance'
   end
+
   # Array iteration for summary STDout
   def statement_body
-    puts @transaction_history.reverse.each { |transaction| transaction }
+    transaction_history.reverse.each { |transaction| puts transaction }
   end
+
   # Credit method to increase balance
-  def credit(transaction_amount)
+  def credit_account(transaction_amount)
     @account_balance += transaction_amount
   end
+
   # Debit method to decrease balance
-  def debit(transaction_amount)
+  def debit_account(transaction_amount)
     @account_balance -= transaction_amount
   end
+
   # Formats the date of transaction
   def transaction_date
-    current_date = Time.now
-    current_date.strftime("%d/%m/%Y")
+    Time.now.strftime('%d/%m/%Y')
   end
+
   # Pushes transactions into an array to view historically
   def save_transaction(new_transaction)
     @transaction_history.push(new_transaction)
+  end
+
+  # Raises error in the event of a value less than 0 and an integer
+  def validate_amount(transaction_amount)
+    raise 'Balance must be above 0!' if transaction_amount <= 0
+  end
+
+  # Formats the print style for credit entries
+  def withdrawal_format(transaction_amount)
+    "#{transaction_date} || || #{add_two_decimals(transaction_amount)} || #{add_two_decimals(@account_balance)}"
+  end
+
+  # Formats the print style for withdrawal entries
+  def deposit_format(transaction_amount)
+    "#{transaction_date} || #{add_two_decimals(transaction_amount)} || || #{add_two_decimals(@account_balance)}"
   end
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -1,50 +1,59 @@
-require 'bank.rb'
+# frozen_string_literal: true
+
+require 'bank'
 
 describe Bank do
   let(:bank) { Bank.new }
-  let(:time) { Time.now }
+  let(:date) { Time.now.strftime('%d/%m/%Y') }
 
-    it 'initializes with an account balance 0' do 
-      expect(bank.account_balance).to eq 0
-    end
-
-    it 'initializes with an empty array to hold transactions' do
-      expect(bank.transaction_history).to eq []
-    end
-
-    it 'depositing increases account balance' do
-      bank.deposit_transaction(500)
-      expect(bank.account_balance).to eq 500
-    end
-
-    it 'withdrawing lowers the account balance' do
-      bank.withdrawal_transaction(500)
-      expect(bank.account_balance).to eq -500
-    end
-
-    it 'debiting 600 and then crediting 500 leaves a debit balance' do
-      bank.withdrawal_transaction(600)
-      bank.deposit_transaction(500)
-      expect(bank.account_balance). to eq -100
-    end
-
-    it 'stores deposit transaction in an array' do
-      bank.deposit_transaction(500.00)
-      expect(bank.transaction_history).to eq ["02/08/2021 || 500.00 || || 500.00"]
-    end
-
-    it 'deposit transaction' do
-      expect(bank.deposit_transaction(500)).to eq ["02/08/2021 || 500.00 || || 500.00"]
-    end
-
-    it 'withdraw transaction' do
-      expect(bank.withdrawal_transaction(2000)).to eq ["02/08/2021 || || 2000.00 || -2000.00"]
-    end
-
-    it 'prints a statement with various transactions included' do
-      bank.deposit_transaction(500)
-      bank.deposit_transaction(600)
-      expect{bank.statement_summary}.to output("date || credit || debit || balance\n02/08/2021 || 600.00 || || 1100.00\n02/08/2021 || 500.00 || || 500.00\n").to_stdout
-    end
+  it 'initializes with an account balance 0' do
+    expect(bank.account_balance).to eq 0
   end
-  
+
+  it 'initializes with an empty array to hold transactions' do
+    expect(bank.transaction_history).to eq []
+  end
+
+  it 'depositing increases account balance' do
+    bank.deposit_transaction(500)
+    expect(bank.account_balance).to eq 500
+  end
+
+  it 'withdrawing lowers the account balance' do
+    bank.withdrawal_transaction(500)
+    expect(bank.account_balance).to eq(-500)
+  end
+
+  it 'debiting 600 and then crediting 500 leaves a debit balance' do
+    bank.withdrawal_transaction(600)
+    bank.deposit_transaction(500)
+    expect(bank.account_balance).to eq(-100)
+  end
+
+  it 'stores deposit transaction in an array' do
+    bank.deposit_transaction(500.00)
+    expect(bank.transaction_history).to eq ["#{date} || 500.00 || || 500.00"]
+  end
+
+  it 'deposit transaction' do
+    expect(bank.deposit_transaction(500)).to eq ["#{date} || 500.00 || || 500.00"]
+  end
+
+  it 'withdraw transaction' do
+    expect(bank.withdrawal_transaction(2000)).to eq ["#{date} || || 2000.00 || -2000.00"]
+  end
+
+  it 'prints a statement with various transactions included' do
+    bank.deposit_transaction(500)
+    bank.deposit_transaction(600)
+    expect { bank.statement_summary }.to output("date || credit || debit || balance\n#{date} || 600.00 || || 1100.00\n#{date} || 500.00 || || 500.00\n").to_stdout
+  end
+
+  it 'deposit fails if value is 0' do
+    expect { bank.deposit_transaction(0) }.to raise_error('Balance must be above 0!')
+  end
+
+  it 'deposit fails if value is 0' do
+    expect { bank.withdrawal_transaction(0) }.to raise_error('Balance must be above 0!')
+  end
+end
