c3673bee06d61e74e9f2d5b96930ec1dcb4a242c
mikey-roberts <mikey_roberts@outlook.com>
2021-08-02 13:17:33 +0100

    slight refactor

--- lib/bank.rb
+++ lib/bank.rb
@@ -24,16 +24,13 @@ class Bank
   end
   # Actions a bank depoit
   def deposit_transaction(amount)
-    amount = amount
     credit(amount)
-    date_today = date
-    transaction = ["#{date_today} || || #{'%.2f' % amount} || #{'%.2f' % @account_balance}"]
+    transaction = ["#{date} || || #{'%.2f' % amount} || #{'%.2f' % @account_balance}"]
   end
   # Actions a bank withdraw
   def withdrawal_transaction(amount)
-    amount = amount
     debit(amount)
     date_today = date
-    transaction = ["#{date_today} || #{'%.2f' % amount} || || #{'%.2f' % @account_balance}"]
+    transaction = ["#{date} || #{'%.2f' % amount} || || #{'%.2f' % @account_balance}"]
   end
 end
